97edfafe6c617adc72b2f7de9a0405b97497bea3
Steven Rouk <stevenrouk@gmail.com>
2019-10-25 10:27:26 -0600

    Add function to get name of movie from movie id.

---
+++ steven/get_movie_name.py
@@ -0,0 +1,17 @@
+import os
+
+import numpy as np
+import pandas as pd
+
+FILE_DIRECTORY = os.path.split(os.path.realpath(__file__))[0]
+DATA_DIRECTORY = os.path.join(os.path.split(FILE_DIRECTORY)[0], 'data', 'movies')
+
+MOVIES_DF = pd.read_csv(os.path.join(DATA_DIRECTORY, 'movies.csv'))
+
+def get_movie_names(movie_ids):
+    """Expects movie_ids as an int, list or NumPy array. Returns a NumPy array of strings."""
+    if isinstance(movie_ids, int):
+        return MOVIES_DF[MOVIES_DF['movieId'] == movie_ids]['title'].values
+    if isinstance(movie_ids, list):
+        movie_ids = np.array(movie_ids)
+    return MOVIES_DF[MOVIES_DF['movieId'].isin(movie_ids)]['title'].values
