5db2b1ccdc8f75ebdf0529894a1ef315c59ecd5e
Steven Rouk <stevenrouk@gmail.com>
2019-10-25 10:28:49 -0600

    Add examples in documentation.

--- steven/get_movie_name.py
+++ steven/get_movie_name.py
@@ -9,7 +9,20 @@ DATA_DIRECTORY = os.path.join(os.path.split(FILE_DIRECTORY)[0], 'data', 'movies'
 MOVIES_DF = pd.read_csv(os.path.join(DATA_DIRECTORY, 'movies.csv'))
 
 def get_movie_names(movie_ids):
-    """Expects movie_ids as an int, list or NumPy array. Returns a NumPy array of strings."""
+    """Expects movie_ids as an int, list or NumPy array. Returns a NumPy array of strings.
+    
+    Example usage:
+        >>> get_movie_names(1)
+        array(['Toy Story (1995)'], dtype=object)
+
+        >>> get_movie_names([1,2,3])
+        array(['Toy Story (1995)', 'Jumanji (1995)', 'Grumpier Old Men (1995)'],
+            dtype=object)
+
+        >>> get_movie_names(np.array([1,2,3]))
+        array(['Toy Story (1995)', 'Jumanji (1995)', 'Grumpier Old Men (1995)'],
+            dtype=object)
+    """
     if isinstance(movie_ids, int):
         return MOVIES_DF[MOVIES_DF['movieId'] == movie_ids]['title'].values
     if isinstance(movie_ids, list):
