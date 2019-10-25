0b2947099f0e83c234dac8212dfb46ce2e9d9aa5
Steven Rouk <stevenrouk@gmail.com>
2019-10-25 13:04:31 -0600

    bug fix

--- app/app.py
+++ app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify, request
+from flask import Flask, render_template, jsonify, request, redirect, url_for
 app = Flask(__name__)
 
 def get_movies(user_id):
@@ -11,8 +11,15 @@ def index():
 
 @app.route('/movie-recommendations', methods=['GET', 'POST'])
 def movie_recommendations():
-    user_id = int(request.form['user_id'])
-    num_movies = int(request.form['number_of_movies'])
+    user_id = request.form['user_id']
+    if not user_id:
+        return redirect('/')
+    user_id = int(user_id)
+
+    num_movies = request.form['number_of_movies']
+    if not num_movies:
+        num_movies = 10
+    num_movies = int(num_movies)
 
     movies = get_movies(user_id)
     movies = movies[:num_movies]
