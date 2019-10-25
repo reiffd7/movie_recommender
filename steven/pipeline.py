c0814b41dbe6937d2f3f8c0a2874a7d47ced7f00
Steven Rouk <stevenrouk@gmail.com>
2019-10-24 18:21:46 -0600

    Implement full pipeline in if-name-equals-main.

--- steven/pipeline.py
+++ steven/pipeline.py
@@ -1,25 +1,47 @@
+import os
 import sys
+
 import numpy as np
 import pandas as pd
+
 from surprise import AlgoBase, Dataset, BaselineOnly
 from surprise import SVD, SVDpp, NMF
 from surprise.reader import Reader
 from surprise.model_selection.split import train_test_split
 from surprise import accuracy
+from steven.ratings_residuals_histogram import single_histogram, double_histogram
+
+FILE_DIRECTORY = os.path.split(os.path.realpath(__file__))[0]
+DATA_DIRECTORY = os.path.join(os.path.split(FILE_DIRECTORY)[0], 'data', 'movies')
 
 if __name__ == "__main__":
-    df = pd.read_csv('../data/movies/ratings.csv')
+    # Read data
+    df = pd.read_csv(os.path.join(DATA_DIRECTORY, 'ratings.csv'))
+
+    # Drop unneeded column 'timestamp'
     df.drop('timestamp', axis=1, inplace=True)
+
+    # Load the data into the surprise format
     reader = Reader()
     data = Dataset.load_from_df(df, reader=reader)
+
+    # Train ALS model
     print('Using ALS')
     bsl_options = {'method': 'als',
                 'n_epochs': 5,
                 'reg_u': 12,
                 'reg_i': 5
                 }
-
     trainset, testset = train_test_split(data, test_size=0.25)
     algo = BaselineOnly(bsl_options=bsl_options)
     predictions = algo.fit(trainset).test(testset)
-    accuracy.rmse(predictions)
+
+    # Get the RMSE of our predictions
+    rmse = accuracy.rmse(predictions)
+
+    # Get true values and predicted values for our test set
+    y_true = [x.r_ui for x in predictions]
+    y_pred = [x.est for x in predictions]
+
+    # Plot a histogram of our model residuals
+    single_histogram(y_true, y_pred, title=f'Histogram of ALS Model Residuals - RMSE {rmse:.3f}')
